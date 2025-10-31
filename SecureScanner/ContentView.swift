4be7438f839bb29d97e305ee29df7c53c899fe6c
Tim <timiswq@gmail.com>
2025-10-31 14:39:11 +0800

    Initial Commit

---
+++ SecureScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecureScanner
+//
+//  Created by Qing Wang on 2025/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecureScanner/SecureScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureScannerApp.swift
+//  SecureScanner
+//
+//  Created by Qing Wang on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SecureScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
